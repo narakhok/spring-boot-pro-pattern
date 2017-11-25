7fca54d53f713957e0c3b3501b8027761241f8a3
unknown <narak.hok@Recervesmb.sambatfinance.com>
2017-11-26 06:05:48 +0700

    Spring Flolow architecture design

---
+++ src/main/java/com/boot/SpringBootProPatternApplication.java
@@ -0,0 +1,12 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProPatternApplication.class, args);
+	}
+}
